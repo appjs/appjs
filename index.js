3fc58e67ef0d16d5dfda099a7121907b5fd1849c
Morteza Milani <mrtz.milani@googlemail.com>
2012-04-28 11:45:52 +0430

    Adds staticRouter.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 var appjs = require('./build/Release/appjs.node');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+
+appjs.staticRouter = function(rootDir){
+  var root = path.join(process.cwd(),rootDir);
+  
+  var router = function(req,callback){
+  	if(req.method == "GET") {
+
+  	  var urlPath = url.parse(req.url).pathname;
+
+  	  if(urlPath == '/') urlPath = '/index.html';
+
+  	  var filePath = path.join(root,urlPath);
+
+  	  if(fs.statSync(filePath).isFile()){
+  	  	var fileContent = fs.readFileSync(filePath);
+  	  	callback("text/html",fileContent);
+  	  	return;
+  	  }
+  	}
+  	callback("text/html","404");
+  };
+
+  return router;
+
+}
 
 module.exports = appjs;
