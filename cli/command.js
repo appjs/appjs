2d20efb60e544a5d2bdee6a32a059b3ba7dbbb17
Morteza Milani <mrtz.milani@googlemail.com>
2012-06-27 04:20:35 +0430

    Some cleanups

--- cli/command.js
+++ cli/command.js
@@ -1,16 +1,31 @@
-var fs = require('fs');
-var path = require('path');
-var util = require('./util');
-var pack = require('./pack');
-var mkdir = require('mkdirp');
-var config = require('./config');
-var exec = require('child_process').exec;
+var fs    = require('fs')
+  , path  = require('path')
+  , mkdir = require('mkdirp')
+  , util  = require('./util')
+  , config = require('./config')
+  , exec = require('child_process').exec;
 
 var init = exports.init = function(){
+  
+  if( arguments.length > 1 ) {
+    util.abort(config.ERROR.tooMuchArguments);
+  }
+
+  var project_dir = process.cwd();
+
+  if( arguments.length == 1 ) {
+    project_dir = fs.realpathSync(arguments[0]);
+  }
+
+  // TODO create boilerplate
+
+  // TODO create launcher
+
+  util.log('OK',util.log.success);
 
 }
 
-var packageF = exports.package = function(){
+var package = exports.package = function(){
 
 }
 
