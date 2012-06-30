658e20cc4380e22cc693ef495e05687821a74e3f
Morteza Milani <mrtz.milani@googlemail.com>
2012-06-30 16:08:07 +0430

    Throws an exception if node version is incompatible.

--- data/common/appjs/index.js
+++ data/common/appjs/index.js
@@ -4,4 +4,9 @@ var platforms = {
 	linux: 'linux'
 };
 
+var version = parseFloat(process.version.replace(/v/,''));
+
+if( version < 0.8 ) {
+ throw new Error("AppJS needs node v0.8.x");
+}
 module.exports = require('appjs-' + platforms[process.platform]);
