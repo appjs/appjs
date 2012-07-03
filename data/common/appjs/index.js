effff0b48c2110f7a6fa4d1b89b11bdb6447859e
Morteza Milani <mrtz.milani@googlemail.com>
2012-07-03 13:00:30 +0430

    Adds manuall exception for darwin x64

--- data/common/appjs/index.js
+++ data/common/appjs/index.js
@@ -1,12 +1,18 @@
-var platforms = {
+var platform  = process.platform
+  , arch      = process.arch
+  , version   = parseFloat(process.version.replace(/v/,''))
+  , platforms = {
 	win32: 'win',
 	darwin: 'mac',
 	linux: 'linux'
-};
-
-var version = parseFloat(process.version.replace(/v/,''));
+    };
 
 if( version < 0.8 ) {
- throw new Error("AppJS needs node v0.8.x");
+  throw new Error("AppJS needs node v0.8.x");
 }
-module.exports = require('appjs-' + platforms[process.platform]);
+
+if( platform == 'darwin' && arch == 'x64' ) {
+  throw new Error("AppJS needs node x64. You can use precompiled packages that include node ia32.");
+}
+
+module.exports = require('appjs-' + platforms[platform]);
