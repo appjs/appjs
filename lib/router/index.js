appjs/appjs

commit f64640545669e2ef83beefb6a5d3980cf532f073

Clean up the router a bunch

lib/router/index.js
@@ -1,80 +1,61 @@
-var methods      = require('./methods')
-  , Response     = require('./response')
-  , Request      = require('./request')
-  , staticRouter = require('./static_router');
+var Response     = require('./response'),
+    Request      = require('./request'),
+    staticRouter = require('./static-router'),
+    Class        = require('../utils').Class;
 
-var req_handlers = [];
+var methods = ['get', 'post'];
 
-function Next(req, res) {
+var Router = module.exports = new Class({
+  constructor: function Router(){
+    var self = this;
+    this.handlers = {};
+    for (var i=0; i < methods.length; i++) {
+      this.handlers[methods[i]] = [];
+    }
 
-  var index = -1;
+    this.on('request',function(request, callback){
+      request.method = request.method.toLowerCase();
+      var listeners = self.handlers[request.method];
 
-  var method = req.method.toLowerCase();
-
-  var next = function() {
-
-    if( req_handlers[method] && ++index < req_handlers[method].length ) {
-      req_handlers[method][index].apply(null,[req,res,next]);
-    } else {
-      res.send(500,'Internal Server Error');
-    }   
-
-  }
-
-  return next;
-}
-
-module.exports = function router() {
-
-  var self = this;
-
-  methods.forEach(function(method){
-
-    req_handlers[method] = [];
-
-    self[method] = function(route,callback) {
-      
-      var handler = function(req,res,next){
-
-        if( req.method.toLowerCase() == method ) {
-          //TODO match route with pathname.
-          if( req.pathname == route ) {
-            callback.call(null,req,res,next);
-            return;
+      if (listeners && listeners.length) {
+        var response = new Response(callback);
+        var index = -1;
+        request = new Request(request);
+        void function next(){
+          if (++index < listeners.length) {
+            listeners[index](request, response, next);
+          } else {
+            response.send(500, 'Internal Server Error');
           }
-
-        }
-
-        next();
-
-      };
-
-      req_handlers[method].push(handler);
+        }();
+      }
+    });
+    return this;
+  },
+  staticRouter: staticRouter,
+  use: function use(middleware){
+    for (var i=0; i < methods.length; i++) {
+      this.handlers[methods[i]].push(middleware);
     }
-
-    self['all'] = function(route,callback) {
-      methods.forEach(function(method){
-        self[method].call(null,route,callback);
-      })
+    return this;
+  },
+  all: function(route, callback) {
+    for (var i=0; i < methods.length; i++) {
+      this[methods[i]](route, callback);
     }
-
-  });
-
-  this.staticRouter = staticRouter;
-
-  this.use = function(middleware) {
-    methods.forEach(function(method){
-      req_handlers[method].push(middleware);
-    })
+    return this;
   }
+});
 
-  this.on("request",function(req,callback){
-
-    req  = new Request(req);
-    res  = new Response(callback);
-    next = new Next(req,res);
-
-    next();
-  });
-
-}
+methods.forEach(function(method){
+  Router.prototype[method] = function(route, callback) {
+    this.handlers[method].push(function(request, response, next){
+      if (request.method === method && request.pathname === route) {
+        callback(request, response, next);
+      } else {
+        next();
+      }
+    });
+    return this;
+  }
+});

lib/router/request.js
@@ -1,33 +1,25 @@
-var querystring = require('querystring');
-var url = require('url');
+var querystring  = require('querystring'),
+    url          = require('url'),
+    Class        = require('../utils').Class;
 
-function Request(req) {
-
-  var parsedURL = url.parse(req.url,true);
-
-  this.url = req.url;
-  this.headers = req.headers;
-  this.method = req.method;
-  this.pathname = parsedURL.pathname;
-  this.hash = parsedURL.hash;
-  this.params = parsedURL.query;
-  this.postData = querystring.parse(req.post);
-
-  var self = this;
-
-  this.param = function(index) {
-    return self.params[index];
-  }
-
-  this.post = function(index) {
-    return self.postData[index];
-  }
-
-  this.get = function(key) {
+module.exports = new Class({
+  constructor: function Request(request){
+    var parsed = url.parse(request.url, true);
+    this.pathname = request.url;
+    this.headers = request.headers;
+    this.method = request.method;
+    this.pathname = parsed.pathname;
+    this.hash = parsed.hash;
+    this.params = parsed.query;
+    this.data = querystring.parse(request.post);
+  },
+  param: function param(index){
+    return this.params[index];
+  },
+  post: function post(index){
+    return self.data[index];
+  },
+  get: function get(key){
     return self.headers[key];
   }
-
-  return this;
-}
-
-module.exports = Request;
+});

lib/router/response.js
@@ -1,45 +1,72 @@
-var status_text = {
-  200:'OK',
-  304:'Not Modified',
-  400:'Bad Request',
-  401:'Unauthorized',
-  403:'Forbidden',
-  404:'Not Found',
-  500:'Internal Server Error'
-};
+var fs    = require('fs'),
+    path  = require('path'),
+    mime  = require('mime'),
+    Class = require('../utils').Class;
 
-function Response(callback) {
 
-  this.send = function send ( status, mime_type, data ) {
+var statusCodes = require('./http.json').statusCodes;
+var EMPTY = new Buffer(0);
 
-    if(arguments.length == 1) {
-      if(typeof status == 'number') {
-        mime_type = 'text/plain';
-        data = '';
+
+module.exports = new Class({
+  constructor: function Response(callback){
+    this.callback = callback;
+  },
+  send: function send(status, mimetype, data){
+    if (arguments.length === 1) {
+      if (typeof status === 'number') {
+        this.status = status;
       } else {
-        mime_type = 'text/html';
-        data = status;
-        status = 200;
+        this.mimetype = 'text/html';
+        this.data = status;
       }
-    } else if(arguments.length == 2){
-      data = mime_type;
-      mime_type = 'text/plain';
+    } else if (arguments.length === 2){
+      this.status = status;
+      this.data = mimetype;
+    } else {
+      this.status = status;
+      this.mimetype = mimetype;
+      this.data = Buffer.isBuffer(data) ? data : new Buffer(data);
     }
 
-    if(typeof data == 'object' && !(data instanceof Buffer) ) {
-      mime_type = 'application/json';
-      data = JSON.stringify(data);
+    this.dispatch();
+  },
+  dispatch: function dispatch(){
+    this.status    || (this.status = 200);
+    this.mimetype  || (this.mimetype = 'text/plain');
+    this.data      || (this.data = EMPTY);
+    this.responseCode = statusCodes[this.status];
+
+    if (this.data && typeof this.data === 'object' && !Buffer.isBuffer(this.data)) {
+      this.mimetype = 'application/json';
+      this.data = new Buffer(JSON.stringify(this.data));
     }
 
-    if(!(data instanceof Buffer)) {
-      data = new Buffer(data);
+    this.callback(this.status, this.responseCode, this.mimetype, this.data);
+  },
+  sendFile: function sendFile(status, filepath){
+    var self = this;
+    if (arguments.length === 2) {
+      this.path = path.resolve(filepath);
+      this.status = status;
+    } else {
+      this.path = path.resolve(status);
     }
+    this.mimetype = mime.lookup(this.path);
 
-    callback(status,status_text[status],mime_type,data);
+    fs.stat(this.path, function(err, stat){
+      if (err || !stat.isFile()) {
+        self.send(404);
+      } else {
+        fs.readFile(self.path, function(err, buffer){
+          if (err || !stat.isFile()) {
+            self.send(500);
+          } else {
+            self.data = buffer;
+            self.dispatch();
+          }
+        });
+      }
+    });
   }
-
-  return this;
-
-}
-
-module.exports = Response;
+});

lib/router/static-router.js
@@ -1,0 +1,18 @@
+var mime = require('mime'),
+    path = require('path'),
+    fs = require('fs');
+
+module.exports = staticRouter;
+
+function staticRouter(root){
+  root = path.join(process.cwd(), root);
+
+  return function router(request, response, next){
+    if (request.method == 'GET') {
+      var url = request.pathname === '/' ? '/index.html' : request.pathname;
+      response.sendFile(path.join(root, url));
+    } else {
+      next();
+    }
+  };
+}

index.js
@@ -1,20 +1,23 @@
-var path = require('path');
-var bindings = require('./lib/bindings');
-var router = require('./lib/router');
+var path = require('path'),
+    router = require('./lib/router'),
+    bindings = module.exports = require('./lib/bindings');
 
-var Init = bindings.init;
+
+var init = bindings.init;
 
 bindings.init = function() {
+  var app = init.apply(null, arguments);
 
-  var app = Init.apply(null,arguments);
-
-  app.extend = function(mod) {
-    mod.call(this);
+  app.extend = function extend(mod){
+    Object.keys(mod.prototype).forEach(function(key){
+      app[key] = mod.prototype[key];
+    });
+    mod.call(app);
   }
 
   app.extend(router);
 
-  app.on("window_close",function(){
+  app.on("window_close", function(){
     process.nextTick(function(){
       process.exit();
     });
@@ -24,11 +27,10 @@ bindings.init = function() {
 
   app.createWindow = function(url,settings){
 
-    if( settings.icons ) {
-      settings.icons['smaller'] = path.resolve(settings.icons['smaller']);
-      settings.icons['small'] = path.resolve(settings.icons['small']);
-      settings.icons['big'] = path.resolve(settings.icons['big']);
-      settings.icons['bigger'] = path.resolve(settings.icons['bigger']);
+    if (settings.icons) {
+      ['smaller', 'small', 'big', 'bigger'].forEach(function(size){
+        settings.icons[size] = path.resolve(settings.icons[size]);
+      });
     }
 
     return createWindow.call(app,url,settings);
@@ -37,4 +39,4 @@ bindings.init = function() {
   return app;
 }
 
-module.exports = bindings;
+bindings;

lib/router/methods.js
@@ -1,4 +1,0 @@
-module.exports = [
-  'get',
-  'post'
-];

lib/router/static_router.js
@@ -1,48 +1,0 @@
-var mime = require('mime')
-  , path = require('path')
-  , fs = require('fs');
-
-function staticRouter(rootDir){
-  
-  var root = path.join(process.cwd(),rootDir);
-
-  var router = function(req,res,next){
-
-    if(req.method == "GET") {
-
-      urlPath = req.pathname;
-
-      if(urlPath == '/') urlPath = '/index.html';
-
-      var filePath = path.join(root,urlPath);
-
-      fs.stat(filePath,function(err,stat){
-
-        if( err || !stat.isFile() ){
-          next();
-          return;
-        }
-
-        fs.readFile(filePath,function(err,content){
-          
-          if(err) {
-            res.send(500,'Internal Server Error');
-            return;
-          }
-
-          mime_type = mime.lookup(filePath);
-          res.send(200,mime_type,content);
-
-        })
-
-      })
-    } else {
-      next();
-    }
-  };
-
-  return router;
-
-}
-
-module.exports = staticRouter;

lib/utils.js
@@ -1,0 +1,25 @@
+module.exports = {
+  Class: Class
+};
+
+function Class(proto){
+  var Ctor = proto.constructor || (proto.constructor = Object);
+  var Super = proto.super || Object;
+  this.__proto__ = Super.prototype;
+  Ctor.__proto__ = Super;
+  Ctor.prototype = this;
+  Object.getOwnPropertyNames(proto).forEach(function(key){
+    if (key !== 'super') {
+      var desc = Object.getOwnPropertyDescriptor(proto, key);
+      if (key[0] === '_') {
+        desc.enumerable = false;
+        key = key.slice(1);
+      } else if (key === 'constructor') {
+        desc.enumerable = false;
+      }
+
+      Object.defineProperty(this, key, desc);
+    }
+  }, this);
+  return Ctor;
+}

commit ac58684c5393bbb5908b6c95764ef4d64da41ee7

Fixes bugs in routers that were introduced in f64640545669e

lib/router/index.js
@@ -14,13 +14,13 @@ var Router = module.exports = new Class({
     }
 
     this.on('request',function(request, callback){
-      request.method = request.method.toLowerCase();
+
+      var request = new Request(request);
+      var response = new Response(callback);
       var listeners = self.handlers[request.method];
 
       if (listeners && listeners.length) {
-        var response = new Response(callback);
         var index = -1;
-        request = new Request(request);
         void function next(){
           if (++index < listeners.length) {
             listeners[index](request, response, next);
@@ -28,6 +28,8 @@ var Router = module.exports = new Class({
             response.send(500, 'Internal Server Error');
           }
         }();
+      } else {
+        response.send(500, 'Internal Server Error');
       }
     });
     return this;
@@ -50,7 +52,7 @@ var Router = module.exports = new Class({
 methods.forEach(function(method){
   Router.prototype[method] = function(route, callback) {
     this.handlers[method].push(function(request, response, next){
-      if (request.method === method && request.pathname === route) {
+      if (request.method.toLowerCase() === method && request.pathname === route) {
         callback(request, response, next);
       } else {
         next();

lib/router/request.js
@@ -7,7 +7,7 @@ module.exports = new Class({
     var parsed = url.parse(request.url, true);
     this.pathname = request.url;
     this.headers = request.headers;
-    this.method = request.method;
+    this.method = request.method.toLowerCase();
     this.pathname = parsed.pathname;
     this.hash = parsed.hash;
     this.params = parsed.query;
@@ -17,9 +17,9 @@ module.exports = new Class({
     return this.params[index];
   },
   post: function post(index){
-    return self.data[index];
+    return this.data[index];
   },
   get: function get(key){
-    return self.headers[key];
+    return this.headers[key];
   }
 });

lib/router/response.js
@@ -42,6 +42,11 @@ module.exports = new Class({
       this.data = new Buffer(JSON.stringify(this.data));
     }
 
+    if(!Buffer.isBuffer(this.data)) {
+      this.data = new Buffer(this.data);
+    }
+
+
     this.callback(this.status, this.responseCode, this.mimetype, this.data);
   },
   sendFile: function sendFile(status, filepath){
@@ -52,12 +57,12 @@ module.exports = new Class({
     } else {
       this.path = path.resolve(status);
     }
-    this.mimetype = mime.lookup(this.path);
 
     fs.stat(this.path, function(err, stat){
       if (err || !stat.isFile()) {
         self.send(404);
       } else {
+        self.mimetype = mime.lookup(this.path);
         fs.readFile(self.path, function(err, buffer){
           if (err || !stat.isFile()) {
             self.send(500);

lib/router/static-router.js
@@ -8,9 +8,27 @@ function staticRouter(root){
   root = path.join(process.cwd(), root);
 
   return function router(request, response, next){
-    if (request.method == 'GET') {
+    if (request.method == 'get') {
       var url = request.pathname === '/' ? '/index.html' : request.pathname;
-      response.sendFile(path.join(root, url));
+      var filePath = path.join(root, url);
+
+      // NOTICE: Don't use request.sendFile because if the file is not there
+      // we won't be able to call next(). Also do not fs.stat and
+      // then request.sendfile since it causes two fs.stat that is not good.
+      fs.stat(filePath, function(err, stat){
+        if (err || !stat.isFile()) {
+          next()
+        } else {
+          var mimetype = mime.lookup(filePath);
+          fs.readFile(filePath, function(err, buffer){
+            if (err) {
+              response.send(500);
+            } else {
+              response.send(200,mimetype,buffer);
+            }
+          });
+        }
+      });
     } else {
       next();
     }