4e2c1d7540e11e222f17379cdc799857d6255b9e
Brandon Benvie <brandon@bbenvie.com>
2012-07-07 07:02:27 -0400

    fix V8StringToCef

--- src/includes/util.h
+++ src/includes/util.h
@@ -2,6 +2,7 @@
 #define APPJS_UTIL
 
 #include "include/cef_base.h"
+#include "include/cef_v8.h"
 #include <node.h>
 
 namespace appjs {
@@ -13,7 +14,7 @@ WCHAR* V8StringToWCHAR(v8::Handle<v8::String> str);
 char* V8StringToChar(v8::Handle<v8::String> str);
 char* V8StringToFunctionChar(v8::Handle<v8::String> str);
 v8::Local<v8::String> CefStringToV8(const CefString& str);
-CefRefPtr<CefV8Value> V8StringToCef(Handle<Value> str);
+CefRefPtr<CefV8Value> V8StringToCef(v8::Handle<v8::Value> str);
 
 class Settings {
 public:
