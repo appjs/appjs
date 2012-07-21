386a932b6f3a7a201209b9710e61fd0cfd6911b0
Brandon Benvie <brandon@bbenvie.com>
2012-07-20 20:43:22 -0400

    Fix for some issues with flash and slow loading pages

--- src/includes/cef_sync_handler.cpp
+++ src/includes/cef_sync_handler.cpp
@@ -15,10 +15,18 @@ bool AppjsSyncHandler::Execute(const CefString& name,
                                CefRefPtr<CefV8Value>& retval,
                                CefString& exception) {
   REQUIRE_UI_THREAD();
-  Local<Value> argv[] = { CefStringToV8(arguments[0]->GetStringValue()) };
-  Handle<Object> window = ClientHandler::GetNodeWindow(browser_);
-  Local<Function> handler = Local<Function>::Cast(window->Get(String::NewSymbol("onmessage")));
-  retval = V8StringToCef(handler->Call(window, 1, argv));
+  if (browser_.get()) {
+    HandleScope scope;
+    Local<Object> global = Context::GetCurrent()->Global();
+    Local<Value> argv[1] = { CefStringToV8(arguments[0]->GetStringValue()) };
+    Handle<Object> window = ClientHandler::GetNodeWindow(browser_);
+    Local<Function> handler = Local<Function>::Cast(window->Get(String::NewSymbol("onmessage")));
+    Local<Value> result = handler->Call(window, 1, argv);
+    if (result->IsString()) {
+      char* plain = V8StringToChar(result->ToString());
+      retval = CefV8Value::CreateString(plain);
+    }
+  }
   return true;
 }
 
