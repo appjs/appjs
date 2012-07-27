7e009ea25de3ec752affce70d333a876b02362b5
Brandon Benvie <brandon@bbenvie.com>
2012-07-27 01:27:14 -0400

    clean up main example

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -1,23 +1,12 @@
 var app = module.exports = require('appjs');
+var KEY_F12 = process.platform === 'darwin' ? 63247 : 123;
 
 app.serveFilesFrom(__dirname + '/content');
 
 var window = app.createWindow({
   width  : 640,
   height : 460,
-  icons  : __dirname + '/content/icons',
-/***************************** defaults ********************************
-* url            : 'http://appjs', // serve static file root and routers
-* autoResize     : false,          // resizes in response to html content
-* showChrome     : true,           // show border and title bar
-* resizable      : false,          // control if users can resize window
-* disableSecurity: true,           // allow cross origin requests
-* opacity        : 1,              // flat percent opacity for window
-* alpha          : false,          // per-pixel alpha blended (Win & Mac)
-* fullscreen     : false,          // client area covers whole screen
-* left           : -1,             // centered by default
-* top            : -1,             // centered by default
-*************************************************************************/
+  icons  : __dirname + '/content/icons'
 });
 
 window.on('create', function(){
@@ -26,15 +15,11 @@ window.on('create', function(){
   window.frame.center();
 });
 
-var KEY_F12 = process.platform === 'darwin' ? 63247 : 123;
-
 window.on('ready', function(){
   console.log("Window Ready");
   window.require = require;
   window.process = process;
   window.module = module;
-  window.console.log('process', process);
-
   window.addEventListener('keydown', function(e){
     if (e.keyCode === KEY_F12) {
       window.frame.openDevTools();
