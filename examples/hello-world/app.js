79b7c984569758661b5e6d544a490c69f601b317
Simon Horton <sij.horton@gmail.com>
2012-11-06 23:50:07 +0100

    added appjs-platform to installed node_modules in the helloWorld app. Also added a package.json file for the helloWorld example application since it needs it now.

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -30,6 +30,75 @@ if (process.argv.length>2) {
 } else {
 	app.serveFilesFrom(__dirname + '/content');
 
+	var menubar = app.createMenu([{
+	  label:'&File',
+	  submenu:[
+		{
+		  label:'E&xit',
+		  action: function(){
+			window.close();
+		  }
+		}
+	  ]
+	},{
+	  label:'&Window',
+	  submenu:[
+		{
+		  label:'Fullscreen',
+		  action:function(item) {
+			window.frame.fullscreen();
+			console.log(item.label+" called.");
+		  }
+		},
+		{
+		  label:'Minimize',
+		  action:function(){
+			window.frame.minimize();
+		  }
+		},
+		{
+		  label:'Maximize',
+		  action:function(){
+			window.frame.maximize();
+		  }
+		},{
+		  label:''//separator
+		},{
+		  label:'Restore',
+		  action:function(){
+			window.frame.restore();
+		  }
+		}
+	  ]
+	}]);
+
+	menubar.on('select',function(item){
+	  console.log("menu item "+item.label+" clicked");
+	});
+
+	var trayMenu = app.createMenu([{
+	  label:'Show',
+	  action:function(){
+		window.frame.show();
+	  },
+	},{
+	  label:'Minimize',
+	  action:function(){
+		window.frame.hide();
+	  }
+	},{
+	  label:'Exit',
+	  action:function(){
+		window.close();
+	  }
+	}]);
+
+	var statusIcon = app.createStatusIcon({
+	  icon:'./data/content/icons/32.png',
+	  tooltip:'AppJS Hello World',
+	  menu:trayMenu
+	});
+
 	var window = app.createWindow({
 	  width  : 640,
 	  height : 460,
@@ -37,12 +106,14 @@ if (process.argv.length>2) {
 	});
 
 	window.on('create', function(){
+	  console.log("Window Created");
 	  window.frame.show();
 	  window.frame.center();
+	  window.frame.setMenuBar(menubar);
 	});
 
 	window.on('ready', function(){
-	  window.require = require;
+	  console.log("Window Ready");
 	  window.process = process;
 	  window.module = module;
 
@@ -55,4 +126,5 @@ if (process.argv.length>2) {
 		}
 	  });
 	});
+
 }
