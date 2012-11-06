0c6a9eb670397171caeb0ce370e16f52cea1769a
Morteza Milani <mrtz.milani@googlemail.com>
2012-11-06 10:44:49 +0330

    Wrappers for menu and tray

--- examples/hello-world/app.js
+++ examples/hello-world/app.js
@@ -2,50 +2,86 @@ var app = module.exports = require('appjs');
 
 app.serveFilesFrom(__dirname + '/content');
 
-var window = app.createWindow({
-  width  : 640,
-  height : 460,
-  icons  : __dirname + '/content/icons'
-});
-
-var menu = app.createMenu([{
-  label:'_File',
+var menubar = app.createMenu([{
+  label:'&File',
   submenu:[
     {
-      label:'E_xit',
+      label:'E&xit',
       action: function(){
         window.close();
       }
     }
   ]
 },{
-  label:'_Edit',
+  label:'&Window',
   submenu:[
     {
-      label:'Undo'
-    },{
-      label:''//separator
-    },
-    {
-      label:'Copy',
-      action:function(item){
-        console.log("item "+item.label+" clicked");
+      label:'Fullscreen',
+      action:function(item) {
+        window.frame.fullscreen();
+        console.log(item.label+" called.");
       }
     },
     {
-      label:'Paste',
-      action:function(item){
-        console.log("item "+item.label+" clicked");
+      label:'Minimize',
+      action:function(){
+        window.frame.minimize();
+      }
+    },
+    {
+      label:'Maximize',
+      action:function(){
+        window.frame.maximize();
+      }
+    },{
+      label:''//separator
+    },{
+      label:'Restore',
+      action:function(){
+        window.frame.restore();
       }
     }
   ]
 }]);
 
+menubar.on('select',function(item){
+  console.log("menu item "+item.label+" clicked");
+});
+
+var trayMenu = app.createMenu([{
+  label:'Show',
+  action:function(){
+    window.frame.show();
+  },
+},{
+  label:'Minimize',
+  action:function(){
+    window.frame.hide();
+  }
+},{
+  label:'Exit',
+  action:function(){
+    window.close();
+  }
+}]);
+
+var statusIcon = app.createStatusIcon({
+  icon:'./data/content/icons/32.png',
+  tooltip:'AppJS Hello World',
+  menu:trayMenu
+});
+
+var window = app.createWindow({
+  width  : 640,
+  height : 460,
+  icons  : __dirname + '/content/icons'
+});
+
 window.on('create', function(){
   console.log("Window Created");
   window.frame.show();
   window.frame.center();
-  window.frame.setMenuBar(menu);
+  window.frame.setMenuBar(menubar);
 });
 
 window.on('ready', function(){
