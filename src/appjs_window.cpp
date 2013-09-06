4aea1005387b8dd66549ff4707bbb31ce468f55e
Ashish Negi <ashishnegi33@gmail.com>
2013-09-06 11:08:06 +0530

    Removed a line

--- src/appjs_window.cpp
+++ src/appjs_window.cpp
@@ -187,8 +187,7 @@ v8::Handle<Value> Window::SendSync(const Arguments& args) {
         // convert Node V8 string to Cef V8 string
         CefV8ValueList argsOut;
         std::unique_ptr<char[]> uniPtr = V8StringToChar(args[0]->ToString());
-        char* plain = uniPtr.get();
-        argsOut.push_back(CefV8Value::CreateString(plain));
+        argsOut.push_back(CefV8Value::CreateString(uniPtr.get());
 
         // execute window.appjs fuction, passing in the string,
         // then convert the return value from a CefValue to a Node V8 string
